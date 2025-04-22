ad55edb14d09484e71d7a6f856d0dccbbecaec2d
Anvar <anv.sult@gmail.com>
2025-04-21 21:57:18 -0400

    Fixed gradlew error by adding change permission line in all dockerfiles

--- song-service/src/main/java/com/champlain/songservice/utils/exceptions/DuplicateSongTitleException.java
+++ song-service/src/main/java/com/champlain/songservice/utils/exceptions/DuplicateSongTitleException.java
@@ -1,7 +1,20 @@
 package com.champlain.songservice.utils.exceptions;
 
 public class DuplicateSongTitleException extends RuntimeException {
-  public DuplicateSongTitleException(String message) {
-    super(message);
-  }
+
+    public DuplicateSongTitleException(String message) {
+        super(message);
+    }
+
+    public DuplicateSongTitleException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DuplicateSongTitleException songTitleAlreadyExists(String songTitle) {
+        return new DuplicateSongTitleException("Song title already exists: " + songTitle);
+    }
+
+    public DuplicateSongTitleException songTitleAlreadyExists(String songTitle, Throwable cause) {
+        return new DuplicateSongTitleException("Song title already exists: " + songTitle, cause);
+    }
 }

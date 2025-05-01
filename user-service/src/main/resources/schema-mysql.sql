96e3272e5bca5a5f9d4cffb0fe77e10ee96cd52c
Anvar <anv.sult@gmail.com>
2025-05-01 08:29:12 -0400

    Removed individual port access to all microservices

--- user-service/src/main/resources/schema-mysql.sql
+++ user-service/src/main/resources/schema-mysql.sql
@@ -1,12 +1,12 @@
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    user_id VARCHAR(36) NOT NULL,
+     id SERIAL PRIMARY KEY,
+     user_id VARCHAR(36) NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     date_of_birth DATE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     username VARCHAR(75) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL
-);
+    );
